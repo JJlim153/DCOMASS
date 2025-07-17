13fcaabe5bfa9f8e5c381e0a7464f1eab84e8f79
Relationary <rnd@relationary.com>
2025-07-17 11:58:49 +0800

    Added Automatic Rmi Creation

--- src/y3javaassignment1/MainServer.java
+++ src/y3javaassignment1/MainServer.java
@@ -1,26 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package y3javaassignment1;
 
-/**
- *
- * @author Daniellim
- */
 import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
 
 public class MainServer {
     public static void main(String[] args) {
         try {
+            // Ensure your RMI registry is started inside the same JVM process
+            LocateRegistry.createRegistry(1099);
+
+            // Set the hostname to localhost (optional but helps avoid binding issues)
             System.setProperty("java.rmi.server.hostname", "127.0.0.1");
-            PayrollServiceImpl obj = new PayrollServiceImpl();
-            Naming.rebind("PayrollService", obj);
-            System.out.println("PayrollService is running...");
+
+            PayrollService service = new PayrollServiceImpl();
+
+            // Bind the object to the RMI registry
+            Naming.rebind("rmi://localhost/PayrollService", service);
+
+            System.out.println("PayrollService is bound and running.");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
-
